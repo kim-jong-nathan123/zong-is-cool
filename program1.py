080d7208fe5cee674cb0cc849e5074539561659a
kim-jong-nathan123 <mrdercus@gmail.com>
2023-02-13 12:49:34 -0500

    squidward iz cool

---
+++ program1.py
@@ -0,0 +1,9 @@
+from os import system
+
+name = input("what is your name? ")
+age = input("give me you age or i will be mad!")
+height = input("how tall are you")
+hole = input("**hole ")
+print(f"hello {name}! You are {age} years old {hole}  and {height} feet tall. wy name is py im going to hurt you!")
+
+system("figlet ':)'")
---
+++ program2.py
@@ -0,0 +1,25 @@
+from os import system
+import time
+
+RED = "/033[1;32m"
+
+name = input("give me you na!me ")
+print(f"hello {name} my name is py")
+color = input("what the h e duble hockey sticks is your favorite color ")
+house = input("were is your house!" )
+guns = input("how many dogs do you f$#king have you a$$hole")
+answer = input("do you have a cat? ")
+if answer == "no":
+    print("ill come to your house and yo up then give you a birthday cake :)") 
+else:
+    print("wow us dont :(")
+if int(guns) == 7:
+    print("wow that a lot o me ohhhh i got a arsenal of em get ready to have fun")
+else:
+    print("you need more guns")
+if house == "florida":
+    system("figlet I will find your house and dress up in a clown costum and...")
+    system("open nuts.gif")
+    time.sleep(2)
+    system("figlet wish you a happy birthday!")
+    system("play whistle.wav")
